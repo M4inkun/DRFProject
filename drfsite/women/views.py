26424d1cf172e7444ef834e93aa689962b6ba6e7
Temirlan Akhmat <a.temirl4n@gmail.com>
2024-08-16 18:15:29 +0500

    changed serializer and linked it with models

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -10,16 +10,18 @@ from .serializers import WomenSerializer
 
 class WomenAPIView(APIView):
     def get(self, request):
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+        lst = Women.objects.all()
+        return Response({'posts': WomenSerializer(lst, many=True).data})
 
     def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         post_new = Women.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id'],
         )
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': WomenSerializer(post_new).data})
 
 
 # class WomenAPIView(generics.ListAPIView):
